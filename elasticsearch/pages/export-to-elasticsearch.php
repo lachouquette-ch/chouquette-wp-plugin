5ba549028e9640176b686d13f2017e11a58ada65
Fabrice Douchant <fabrice.douchant@gmail.com>
2020-08-03 13:48:20 +0200

    always view stats

--- elasticsearch/pages/export-to-elasticsearch.php
+++ elasticsearch/pages/export-to-elasticsearch.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 use Elasticsearch\ClientBuilder;
 
 $client = ClientBuilder::create()->setHosts(array(Chouquette_WP_Plugin_Elasticsearch::NODE_1))->build();
+$stats = $client->indices()->stats(array('index' => array(Chouquette_WP_Plugin_Elasticsearch_Post::INDEX, Chouquette_WP_Plugin_Elasticsearch_Fiche::INDEX)));
 ?>
 
 <h1>Administration Elasticsearch</h1>
@@ -32,8 +33,6 @@ if (isset($_POST['export-posts'])) {
         }
         $responses = $client->bulk($params);
     }
-    $stats = $client->indices()->stats(array('index' => Chouquette_WP_Plugin_Elasticsearch_Post::INDEX));
-    echo '<p><strong>Result for posts : </strong>' . $stats['_all']['primaries']['docs']['count'] . ' indexed / ' . $post_query->found_posts . ' posts</p>';
 } elseif (isset($_POST['export-fiches'])) {
     $fiche_query = new WP_Query(array(
         'post_type' => 'fiche',
@@ -57,11 +56,15 @@ if (isset($_POST['export-posts'])) {
         }
         $responses = $client->bulk($params);
     }
-    $stats = $client->indices()->stats(array('index' => Chouquette_WP_Plugin_Elasticsearch_Fiche::INDEX));
-    echo '<p><strong>Result for fiches : </strong>' . $stats['_all']['primaries']['docs']['count'] . ' indexed / ' . $fiche_query->found_posts . ' posts</p>';
 }
 ?>
 
+<h2>Statistiques des indices</h2>
+<ul>
+    <li><?php print($stats['indices'][Chouquette_WP_Plugin_Elasticsearch_Post::INDEX]['total']['docs']['count'] . ' posts indexés') ?></li>
+    <li><?php print($stats['indices'][Chouquette_WP_Plugin_Elasticsearch_Fiche::INDEX]['total']['docs']['count'] . ' fiches indexées') ?></li>
+</ul>
+<h2>Action sur les indices</h2>
 <form method='POST'>
     <p class='submit'>
         <input type='submit' name='export-posts' class='button button-primary' value='Exporter les articles'/>
